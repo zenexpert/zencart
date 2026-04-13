37e0f6dbc7086fe803d01e66ae13c07a3bf353d1
Chris Brown <code@drbyte.dev>
2026-04-13 13:00:02 -0400

    Set default blank

--- zc_install/sql/install/mysql_zencart.sql
+++ zc_install/sql/install/mysql_zencart.sql
@@ -2071,7 +2071,7 @@ CREATE TABLE reviews_description (
   reviews_id int(11) NOT NULL default '0',
   languages_id int(11) NOT NULL default '0',
   reviews_text text NOT NULL,
-  reviews_title VARCHAR(128) NOT NULL,
+  reviews_title VARCHAR(128) NOT NULL DEFAULT '',
   PRIMARY KEY  (reviews_id,languages_id)
 ) ENGINE=MyISAM;
 
